582691a74ca04438c13aafb834f3f23f82775291
Karpagambigai <36296018+Karpagambigai@users.noreply.github.com>
2018-05-24 23:18:31 +0530

    Add files via upload

--- services/goodreadsService.js
+++ services/goodreadsService.js
@@ -1,1 +1,31 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
 
+const parser = xml2js.Parser({ explicitArray: false });
+
+function goodreadsService() {
+    function getBookById(bookId) {
+        return new Promise((resolve, reject) => {
+            axios.get(`https://www.goodreads.com/book/show/${bookId}.xml?key=JcKC5ts6yr83qRwEwJ3w`)
+                .then((response) => {
+                    parser.parseString(response.data, (err, result) => {
+                        if (err) {
+                            debug(err);
+                        } else {
+                            debug(result);
+                            resolve(result.GoodreadsResponse.book);
+                        }
+                    });
+                })
+                .catch((error) => {
+                    reject(error);
+                    debug(error);
+                });
+        });
+    }
+
+    return { getBookById };
+}
+
+module.exports = goodreadsService();
