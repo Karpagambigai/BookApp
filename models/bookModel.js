d813f662f2317ae6301733a8d781bc9b2154a74c
Karpagambigai <36296018+Karpagambigai@users.noreply.github.com>
2018-05-24 23:12:58 +0530

    Add files via upload

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
+const bookmodel = new Schema({
+  author: { type: String },
+  title: { type: String },
+  read: { type: Boolean, default: false },
+  genre: { type: String },
+});
+
+module.exports = mongoose.model('Book', bookmodel);
